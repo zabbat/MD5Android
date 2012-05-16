945ec14b2f7e739e80c966b5331e153b67e4ea9e
Jungbeck <jungbeck82@gmail.com>
2012-05-16 22:42:38 +0200

    adds two customized buttons to gl surface

--- gen/net/wandroid/md5/R.java
+++ gen/net/wandroid/md5/R.java
@@ -22,15 +22,17 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
+        public static final int centerPoint=0x7f070001;
         public static final int gl2view=0x7f070000;
     }
     public static final class layout {
         public static final int glmain=0x7f030000;
-        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
         public static final int hello=0x7f050000;
+        public static final int main_button_left=0x7f050002;
+        public static final int main_button_right=0x7f050003;
     }
     public static final class style {
         public static final int button_text=0x7f060000;
