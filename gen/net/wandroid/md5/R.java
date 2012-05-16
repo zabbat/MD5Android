214768d2d8a86eb151e86c6672208704305b548e
Jungbeck <jungbeck82@gmail.com>
2012-05-16 22:28:42 +0200

    adds customized buttons

--- gen/net/wandroid/md5/R.java
+++ gen/net/wandroid/md5/R.java
@@ -10,18 +10,29 @@ package net.wandroid.md5;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int gradient_end=0x7f040003;
+        public static final int gradient_start=0x7f040002;
+        public static final int pushed_solid=0x7f040000;
+        public static final int pushed_stroke=0x7f040001;
+        public static final int stroke=0x7f040004;
+    }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int button_shape=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int gl2view=0x7f050000;
+        public static final int gl2view=0x7f070000;
     }
     public static final class layout {
         public static final int glmain=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+    }
+    public static final class style {
+        public static final int button_text=0x7f060000;
     }
 }
